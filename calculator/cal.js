b544c427f4e4fe42124489c0afd41e18f90f7b6f
Suman Sapkota <suman2020j@gmail.com>
2023-06-05 00:33:34 +0545

    Calculator

---
+++ calculator/cal.js
@@ -0,0 +1,23 @@
+let string="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else{
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value=string;
+        }
+        
+        }
+    )
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == 'Back'){
+            string=string.slice(0, -1);
+            document.querySelector('input').value=string;
+
+        }
+    })
+})
